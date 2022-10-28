7fbced91468d0a86732c0a1f10a6fa9cfeb920ed
passenger3e <ali_vara_01@hotmail.com>
2022-10-28 20:25:22 +0300

    Initial Commit

---
+++ src/StaticInitBlocks.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class StaticInitBlocks {
+    static int b,h,area;
+    static boolean flag=false;
+    static {
+        Scanner scan =new Scanner(System.in);
+        b=scan.nextInt();
+        h=scan.nextInt();
+        if(b<=0||h<=0){
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+            //throw new Exception("java.lang.Exception: Breadth and height must be positive");
+        }else{
+            flag=true;
+            area=b*h;
+            System.out.println(area);
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
+
